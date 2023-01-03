94d64b95f2304337bf6953a475af0a519f20704e
Rachel Grant <rachel.grant:@derivco.co.im>
2023-01-03 11:13:12 +0000

    add Astrological Signs kata code to git

---
+++ AstrologicalSign.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AstrologicalSignsKata
+{
+  public class AstrologicalSign
+  {
+    public DateTime EndDate;
+    public string Name;
+
+    public AstrologicalSign(string name, DateTime endDate)
+    {
+      Name = name;
+      EndDate = endDate;
+    }
+
+    public static List<AstrologicalSign> CreateSigns()
+    {
+      List<AstrologicalSign> SignList = new List<AstrologicalSign>();
+      SignList.Add(new AstrologicalSign("Capricon", DateTime.Parse("January 20 2000")));
+      SignList.Add(new AstrologicalSign("Aquarius", DateTime.Parse("February 19 2000")));
+      SignList.Add(new AstrologicalSign("Pisces", DateTime.Parse("March 20 2000")));
+      SignList.Add(new AstrologicalSign("Aires", DateTime.Parse("April 20 2000")));
+      SignList.Add(new AstrologicalSign("Taurus", DateTime.Parse("May 20 2000")));
+      SignList.Add(new AstrologicalSign("Gemini", DateTime.Parse("June 21 2000")));
+      SignList.Add(new AstrologicalSign("Cancer", DateTime.Parse("July 22 2000")));
+      SignList.Add(new AstrologicalSign("Leo", DateTime.Parse("August 22 2000")));
+      SignList.Add(new AstrologicalSign("Virgo", DateTime.Parse("September 21 2000")));
+      SignList.Add(new AstrologicalSign("Libra", DateTime.Parse("October 22 2000")));
+      SignList.Add(new AstrologicalSign("Scorpio", DateTime.Parse("November 22 2000")));
+      SignList.Add(new AstrologicalSign("Sagittarius", DateTime.Parse("December 21 2000")));
+      return SignList;
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AstrologicalSignsKata
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Get input from console
+      List<string> inputList = new List<string>();
+      var numInputLines = Convert.ToInt32(Console.ReadLine());
+      for (int i = 0; i < numInputLines; i++)
+      {
+        var line = Console.ReadLine();
+        inputList.Add(line);
+      }
+      
+      // Get Signs
+      var signList = AstrologicalSign.CreateSigns();
+
+      // Check for bday in signs
+      foreach (var bday in inputList)
+      {
+        var withYear = $"{bday} 2000";
+        var birthdayDT = DateTime.Parse(withYear);
+
+        var sign = signList.Find(x => birthdayDT.CompareTo(x.EndDate) <= 0);
+        Console.WriteLine(sign.Name);
+      }
+    }
+  }
+}
